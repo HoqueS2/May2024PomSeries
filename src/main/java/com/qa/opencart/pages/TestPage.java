a1c60ac55234c23089a8dee09446e39e4fcb3ab6
HoqueS2 <59152849+HoqueS2@users.noreply.github.com>
2024-10-02 20:31:16 -0400

    Create TestPage.java

---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class TestPage.java {
+	
+	By name = By.id("klo");
+	
+	public void findTestPage () {
+		System.out.println("Test Page");
+		
+	}
+
+}
