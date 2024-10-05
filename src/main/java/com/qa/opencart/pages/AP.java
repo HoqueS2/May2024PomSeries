d94fbc18e97351ddfea5d053cbf9ba662a38b635
Sharmin Hoque <sharminhoque@Sharmins-MBP.lan>
2024-10-05 11:57:51 -0400

    latest update

--- src/main/java/com/qa/opencart/pages/AP.java
+++ src/main/java/com/qa/opencart/pages/AP.java
@@ -1,5 +1,12 @@
 package com.qa.opencart.pages;
 
 public class AP {
+	
+	// Working in localy with my assign work on AP page 
+	
+	public void findAPPAge () {
+		System.out.println("find my Ap Page");
+		
+	}
 
 }
